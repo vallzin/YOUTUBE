f418a357718cfba00e6c135674df68cc0417fc60
vallzin <valmilson@gmail.com>
2024-07-14 22:22:22 -0300

    adicionado repositorio

---
+++ LOIANNE GRONER/data-binding/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppBootstrapModule } from './app-bootstrap-module/app-bootstrap-module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    AppBootstrapModule
+  ]
+})
+export class AppModule { }
