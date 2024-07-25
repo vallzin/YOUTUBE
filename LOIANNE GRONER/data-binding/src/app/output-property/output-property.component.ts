0ce28fcb1b7db2e55de4a58b0c7199ae772c4cb7
vallzin <valmilson@gmail.com>
2024-07-25 19:59:25 -0300

    acesso DOM e tamplate ViewChild

--- LOIANNE GRONER/data-binding/src/app/output-property/output-property.component.ts
+++ LOIANNE GRONER/data-binding/src/app/output-property/output-property.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'contador',
@@ -11,13 +11,17 @@ export class OutputPropertyComponent implements OnInit{
 
   @Output() mudouValor = new EventEmitter();
 
+  @ViewChild('campoInput') campoValorInput!: ElementRef;
+
   incrementa(){
-    this.valor++
+    console.log(this.campoValorInput.nativeElement.value++);
+    // this.valor++
     this.mudouValor.emit({novoValor: this.valor})
   }
 
   decrementa(){
-    this.valor--
+    // this.valor--
+    console.log(this.campoValorInput.nativeElement.value--);
     this.mudouValor.emit({novoValor: this.valor})
   }
 
