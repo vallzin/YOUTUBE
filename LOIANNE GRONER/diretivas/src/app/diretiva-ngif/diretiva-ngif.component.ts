3cf5e6ffbafd6aa0e040f2197bf2d47f29430bf1
vallzin <valmilson@gmail.com>
2024-07-31 17:40:21 -0300

    hidden diretiva ngif

--- LOIANNE GRONER/diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ LOIANNE GRONER/diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-diretiva-ngif',
   templateUrl: './diretiva-ngif.component.html',
   styleUrl: './diretiva-ngif.component.css'
 })
-export class DiretivaNgifComponent {
+export class DiretivaNgifComponent implements OnInit{
+
+  cursos: string[] = ["Angular 17"];
+
+  mostrarCursos: boolean = false;
+
+  constructor(){
+  }
+  ngOnInit(): void {
+  }
+
+  onMostrarCursos(){
+    this.mostrarCursos = !this.mostrarCursos;
+  }
 
 }
