83374a52bded87dbc4914143051b0a68baedc141
vallzin <valmilson@gmail.com>
2024-08-09 18:58:43 -0300

    finalizando primeira etapa

--- RALF-LIMA/angular-basico/src/app/component11/component11.component.ts
+++ RALF-LIMA/angular-basico/src/app/component11/component11.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Pessoa } from '../modelo/Pessoa';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-component11',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule
+  ],
   templateUrl: './component11.component.html',
   styleUrl: './component11.component.css'
 })
@@ -28,7 +32,62 @@ export class Component11Component {
   //visibilidade dos botões
   btnCadastrar:boolean = true;
 
+  //armazenar índice da pessoa selecionada
+  indice:number = -1;
+
   //vetor
   vetor:Pessoa[] = [];
+
+  //função de cadastro
+  cadastrar(){
+    //cadastro no vetor
+    this.vetor.push(this.formulario.value as Pessoa);
+    //limpeza dos inputs
+    this.formulario.reset();
+    //visualização via console
+    // console.table(this.vetor);
+  }
+
+  //função de seleção
+  selecionar(indice:number){
+    //atribuir o índice da pessoa
+    this.indice = indice;
+    //atribuir os dados da pessoa no formulário
+    this.formulario.setValue({
+      nome  : this.vetor[indice].nome,
+      idade : this.vetor[indice].idade,
+      cidade: this.vetor[indice].cidade
+    });
+    //visibilidade dos botões
+    this.btnCadastrar = false;
+  }
+
+  //função de alteração
+  alterar(){
+    //alterar vetor
+    this.vetor[this.indice] = this.formulario.value as Pessoa;
+    //limpeza os inputs
+    this.formulario.reset();
+    //visibilidade dos botões
+    this.btnCadastrar = true;
+  }
+
+  //função de remoção
+  remover(){
+    //removendo pessoa do vetor
+    this.vetor.splice(this.indice, 1);
+    //limpeza dos inputs
+    this.formulario.reset();
+    //visibilidade dos botões
+    this.btnCadastrar = true;
+  }
+
+  //função cancelar
+  cancelar(){
+    //limpeza dos inputs
+    this.formulario.reset;
+    //visibilidade dos botões
+    this.btnCadastrar = true;
+  }
 
 }
--- RALF-LIMA/angular-basico/src/app/modelo/Pessoa.ts
+++ RALF-LIMA/angular-basico/src/app/modelo/Pessoa.ts
@@ -1,5 +1,5 @@
 export class Pessoa{
-  nome:String;
+  nome:string;
   idade:number;
   cidade:string;
 }
