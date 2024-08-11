685adbbedc88e6ba360b5482c66e9f57f2d6856f
vallzin <valmilson@gmail.com>
2024-08-11 18:47:47 -0300

    api restfull com spring boot start

---
+++ LUCAS-ANGELO/todosimples/src/main/java/br/com/vallzin/todosimples/TodosimplesApplication.java
@@ -0,0 +1,13 @@
+package br.com.vallzin.todosimples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodosimplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodosimplesApplication.class, args);
+	}
+
+}
---
+++ LUCAS-ANGELO/todosimples/src/test/java/br/com/vallzin/todosimples/TodosimplesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vallzin.todosimples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosimplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
