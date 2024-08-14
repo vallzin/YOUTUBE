d3a522f7fffa971e4f1772830d9cca88c80b4477
vallzin <valmilson@gmail.com>
2024-08-14 19:25:28 -0300

    Creado TaskController

--- LUCAS-ANGELO/todosimples/src/main/java/br/com/vallzin/todosimples/controllers/TaskController.java
+++ LUCAS-ANGELO/todosimples/src/main/java/br/com/vallzin/todosimples/controllers/TaskController.java
@@ -1,6 +1,7 @@
 package br.com.vallzin.todosimples.controllers;
 
 import java.net.URI;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,47 +16,50 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import br.com.vallzin.todosimples.models.User;
-import br.com.vallzin.todosimples.models.User.CreateUser;
-import br.com.vallzin.todosimples.models.User.UpdateUser;
-import br.com.vallzin.todosimples.services.UserService;
+import br.com.vallzin.todosimples.models.Task;
+import br.com.vallzin.todosimples.services.TaskService;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/task")
 @Validated
-public class UserController {
-    
+public class TaskController {
+
     @Autowired
-    private UserService userService;
+    private TaskService taskService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> findById(@PathVariable Long id){
-        User obj = this.userService.findById(id);
-        return ResponseEntity.ok().body(obj);
+    private ResponseEntity<Task> findById(@PathVariable Long id){
+        Task obj = this.taskService.findById(id);
+        return ResponseEntity.ok(obj);
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Task>> findAllByUserId(@PathVariable Long userId){
+        List<Task> objs = this.taskService.findAllByUserId(userId);
+        return ResponseEntity.ok().body(objs);
     }
 
     @PostMapping
-    @Validated(CreateUser.class)
-    public ResponseEntity<Void> create(@Valid @RequestBody User obj){
-        this.userService.create(obj);
+    @Validated
+    public ResponseEntity<Void> create(@Valid @RequestBody Task obj){
+        this.taskService.create(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
 
     @PutMapping("/{id}")
-    @Validated(UpdateUser.class)
-    public ResponseEntity<Void> update(@Valid @RequestBody User obj, @PathVariable Long id){
+    @Validated
+    public ResponseEntity<Void> update(@Valid @RequestBody Task obj, @PathVariable Long id){
         obj.setId(id);
-        obj = this.userService.update(obj);
+        this.taskService.update(obj);
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id){
-        this.userService.delete(id);
+        this.taskService.delete(id);
         return ResponseEntity.noContent().build();
     }
-
-
+    
 }
--- LUCAS-ANGELO/todosimples/src/main/java/br/com/vallzin/todosimples/services/TaskService.java
+++ LUCAS-ANGELO/todosimples/src/main/java/br/com/vallzin/todosimples/services/TaskService.java
@@ -1,5 +1,6 @@
 package br.com.vallzin.todosimples.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,11 @@ public class TaskService {
     public Task findById(Long id){
         Optional<Task> task = this.taskRepository.findById(id);
         return task.orElseThrow(() -> new RuntimeException("Tarefa não encontrada! Id: "+id+", Tipo: "+ Task.class.getName()));
+    }
+
+    public List<Task> findAllByUserId(Long userId){
+        List<Task> tasks = this.taskRepository.findByUser_Id(userId);
+        return tasks;
     }
 
     @Transactional
