101eef115c430e540055299e07b9f7ad369eb748
vallzin <valmilson@gmail.com>
2024-10-06 21:33:03 -0300

    exercicios loiane

---
+++ LOIANNE GRONER/JAVA/primeiro-projeto/src/com/loiane/cursojava/repeticao/exercicios/Aula17Exercicio01.java
@@ -0,0 +1,25 @@
+package com.loiane.cursojava.repeticao.exercicios;
+
+import java.util.Scanner;
+
+public class Aula17Exercicio01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite  uma nota entre zero e dez");
+        int nota = sc.nextInt();
+
+        boolean faca = false;
+        do {
+            if(nota >= 0 && nota <= 10){
+                faca = true;
+                System.out.println("Você digitou: " + nota);
+
+            }else {
+                System.out.println("Nota inválida, digite novamente");
+                nota = sc.nextInt();
+            }
+        }while(!faca);
+
+    }
+}
