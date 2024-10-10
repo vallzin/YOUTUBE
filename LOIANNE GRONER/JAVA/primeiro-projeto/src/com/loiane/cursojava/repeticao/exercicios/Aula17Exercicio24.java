239383c1a888d18b916283ee62e881d2f14eaed9
vallzin <valmilson@gmail.com>
2024-10-10 20:07:53 -0300

    exercícios loiane parte3

---
+++ LOIANNE GRONER/JAVA/primeiro-projeto/src/com/loiane/cursojava/repeticao/exercicios/Aula17Exercicio24.java
@@ -0,0 +1,27 @@
+package com.loiane.cursojava.repeticao.exercicios;
+
+
+import java.util.Scanner;
+
+public class Aula17Exercicio24 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite a quantidade de notas:");
+        int n = sc.nextInt();
+
+        double soma = 0;
+        double media;
+        double nota;
+        for (int i = 0; i < n; i++){
+            System.out.println("Entre com a nota "+ (i + 1));
+            nota = sc.nextDouble();
+
+            soma += nota;
+        }
+        System.out.println("Soma = "+ soma);
+        System.out.println("Média = "+ (soma / n));
+
+    }
+}
+
---
+++ LOIANNE GRONER/JAVA/primeiro-projeto/src/com/loiane/cursojava/repeticao/exercicios/Aula17Exercicio25.java
@@ -0,0 +1,37 @@
+package com.loiane.cursojava.repeticao.exercicios;
+
+
+import java.util.Scanner;
+
+public class Aula17Exercicio25 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o número de pessoas");
+        int n = sc.nextInt();
+
+        int idade;
+        int denominado = n;
+        double soma = 0;
+        double mediaIdade;
+        do {
+            System.out.print("Digite sua idade: ");
+            idade = sc.nextInt();
+            soma += idade;
+            mediaIdade = soma / denominado;
+            n--;
+
+        }while (n != 0);
+
+        if(mediaIdade > 0 && mediaIdade < 25){
+            System.out.println("A turma é jovem");
+        } else if (mediaIdade > 25 && mediaIdade < 60) {
+            System.out.println("A turma é adulta");
+        } else if (mediaIdade > 60) {
+            System.out.println("A turma é idosa");
+        }
+
+
+    }
+}
+
