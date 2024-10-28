ecfaf3c7491c087edf526b81690945d4ec617f6c
vallzin <valmilson@gmail.com>
2024-10-28 14:40:43 -0300

    loiane introdução OO Classes e Atributos

---
+++ LOIANNE GRONER/JAVA/primeiro-projeto/src/com/loiane/cursojava/orientcaoobjeto/classesatributos/Carro.java
@@ -0,0 +1,12 @@
+package com.loiane.cursojava.orientcaoobjeto.classesatributos;
+
+public class Carro {
+
+    String marca;
+    String modelo;
+    int numPassageiro;
+    double capCombustivel;
+    double consCombustivel;
+
+
+}
---
+++ LOIANNE GRONER/JAVA/primeiro-projeto/src/com/loiane/cursojava/orientcaoobjeto/classesatributos/TestCarro.java
@@ -0,0 +1,30 @@
+package com.loiane.cursojava.orientcaoobjeto.classesatributos;
+
+public class TestCarro {
+    public static void main(String[] args) {
+
+        Carro van = new Carro();
+
+        van.marca = "Fiat";
+        van.modelo = "Ducato";
+        van.numPassageiro = 10;
+        van.capCombustivel = 100;
+        van.consCombustivel = 0.2;
+
+        System.out.println(van.marca);
+        System.out.println(van.modelo);
+
+        System.out.println();
+
+        Carro fusca = new Carro();
+
+        fusca.marca = "VolksWagen";
+        fusca.modelo = "Fusca";
+        fusca.numPassageiro = 4;
+        fusca.capCombustivel = 40;
+        fusca.consCombustivel = 1.3;
+
+        System.out.println(fusca.marca);
+        System.out.println(fusca.modelo);
+    }
+}
