a2b854f2089da1a92f79ebddeef466868faa24c3
vallzin <valmilson@gmail.com>
2024-10-29 14:21:16 -0300

    devdojo Orientção a Obj. Métodos parte01

---
+++ DEVDOJO/maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest01 {
+    public static void main(String[] args) {
+
+        Calculadora calculadora = new Calculadora();
+
+        calculadora.somaDoisNumeros();
+        System.out.println("Finalizando CalculadoraTest01");
+
+        System.out.println();
+        calculadora.subtraiDoisNumeros();
+
+    }
+}
