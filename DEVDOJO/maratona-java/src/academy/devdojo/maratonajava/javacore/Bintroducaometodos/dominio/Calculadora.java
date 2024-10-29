0d0364af7142bd59912765fc58b9688772382584
vallzin <valmilson@gmail.com>
2024-10-29 14:48:55 -0300

    passangem de parâmentro e argumentos

--- DEVDOJO/maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ DEVDOJO/maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -10,4 +10,9 @@ public class Calculadora {
         System.out.println(21-2);
     }
 
+    public void multiplicaDoisNumeros(int numParaMultiplicar, int num2){//dentro dos parêntesis estão os parâmentros
+
+        System.out.println(numParaMultiplicar * num2);
+
+    }
 }
