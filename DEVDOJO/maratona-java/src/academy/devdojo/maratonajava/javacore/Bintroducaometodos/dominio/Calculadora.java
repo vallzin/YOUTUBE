11fa2a97b5fc2778d2bfb2a3a9ad7329dbac2869
vallzin <valmilson@gmail.com>
2024-10-29 16:59:30 -0300

    devdojo Orientação a Obj. Métodos parte04 Retorno parte02

--- DEVDOJO/maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ DEVDOJO/maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -27,5 +27,31 @@ public class Calculadora {
         }
         return num1/num2;
     }
+    public double divideDoisNumeros4(double num1, double num2) {
+        if (num2 == 0) {
+            return 0;
+        } else{
+            return num1 / num2;
+        }
+    }
+
+    public double divideDoisNumeros5(double num1, double num2) {
+        if (num2 != 0) {
+            return num1 / num2;
+        } else{
+            return 0;
+        }
+    }
+
+    public void imprimeDivisaoDoisNumeros(double num1, double num2){
+        //não tem retorno por ser "void"
+        if (num2 == 0) {
+            System.out.println("Não existe divisão por zero");
+            return;
+        }
+            System.out.println(num1 / num2);
+
+    }
+
 
 }
--- DEVDOJO/maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
+++ DEVDOJO/maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
@@ -4,6 +4,7 @@ import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculad
 
 public class CalculadoraTest03 {
     public static void main(String[] args) {
+
         Calculadora calculadora = new Calculadora();
         double resultado1 = calculadora.divideDoisNumeros(20, 2);
         System.out.println(resultado1);
@@ -21,6 +22,20 @@ public class CalculadoraTest03 {
         double resultado4 = calculadora3.divideDoisNumeros3(20,0);
         System.out.println(resultado4);
 
+        Calculadora calculadora4 = new Calculadora();
+        double resultado5 = calculadora4.divideDoisNumeros4(20,0);
+        System.out.println(resultado5);
+
+        Calculadora calculadora5 = new Calculadora();
+        double resultado6 = calculadora5.divideDoisNumeros5(20,2);
+        System.out.println(resultado6);
+
+        System.out.println("-------------------------");
+        Calculadora calculadora6 = new Calculadora();
+//        double resultado7 = calculadora6.imprimeDivisaoDoisNumeros(80,0);//erro de compilação
+        calculadora6.imprimeDivisaoDoisNumeros(40,4);
+
 
     }
+
 }
