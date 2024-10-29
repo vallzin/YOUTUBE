04a3af051beca089b9af710ecf23cfc646b74dd4
vallzin <valmilson@gmail.com>
2024-10-29 19:09:02 -0300

    Orientação a Obj. parte07 Parâmetros tipo referência part02

--- DEVDOJO/maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Estudante.java
+++ DEVDOJO/maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Estudante.java
@@ -6,4 +6,11 @@ public class Estudante {
     public int idade;
     public char  sexo;
 
+    public void imprime(){
+        System.out.println("-------------------------");
+        System.out.println(this.nome);
+        System.out.println(this.sexo);
+        System.out.println(this.idade);
+    }
+
 }
