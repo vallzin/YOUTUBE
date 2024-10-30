7d22edfa684436f7050739c08c8e920e3914118c
vallzin <valmilson@gmail.com>
2024-10-30 19:20:19 -0300

    loiane Orientação a Obj. 'classes e métodos com parâmetros'

---
+++ LOIANNE GRONER/JAVA/primeiro-projeto/src/com/loiane/cursojava/orientcaoobjeto/classesmetodoparametros/Carro.java
@@ -0,0 +1,26 @@
+package com.loiane.cursojava.orientcaoobjeto.classesmetodoparametros;
+
+public class Carro {
+
+    public String marca;
+    public String modelo;
+    public int numPassageiros;
+    public double capCombustivel;
+    public double consCombustivel;
+
+    public void exibirAutonomia(){
+        System.out.println("A autonomia do carro é: "+ capCombustivel * consCombustivel + "Km");
+    }
+
+    double obterAutonomia(){
+        System.out.println("Método obterAutonomia foi chamado");
+        return capCombustivel * consCombustivel;
+    }
+
+    double calcularCombustivel(double km){
+        double qtdCombustivel = km / consCombustivel;
+        return qtdCombustivel;
+    }
+
+
+}
