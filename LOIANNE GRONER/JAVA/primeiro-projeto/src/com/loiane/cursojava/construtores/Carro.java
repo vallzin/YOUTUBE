454b476554e9e5d8464213a6e4fe26006b46af8b
vallzin <valmilson@gmail.com>
2024-11-01 16:47:38 -0300

    loiane construtores

---
+++ LOIANNE GRONER/JAVA/primeiro-projeto/src/com/loiane/cursojava/construtores/Carro.java
@@ -0,0 +1,37 @@
+package com.loiane.cursojava.construtores;
+
+public class Carro {
+    public String marca;
+    public String modelo;
+    public int numPassageiros;
+    public double capCombustivel;
+    public double consCombustivel;
+
+    Carro(){
+        System.out.println("Classe carro foi intanciada");
+        numPassageiros = 4;
+    }
+
+    Carro(String marca_, String modelo_, int numPassageiros_,double capCombustivel_, double consCombustivel_ ){
+        marca = marca_;
+        modelo = modelo_;
+        numPassageiros = numPassageiros_;
+        capCombustivel = capCombustivel_;
+        consCombustivel = consCombustivel_;
+    }
+
+    public void exibirAutonomia(){
+        System.out.println("A autonomia do carro é: "+ capCombustivel * consCombustivel + "Km");
+    }
+
+    double obterAutonomia(){
+        System.out.println("Método obterAutonomia foi chamado");
+        return capCombustivel * consCombustivel;
+    }
+
+    double calcularCombustivel(double km){
+        double qtdCombustivel = km / consCombustivel;
+        return qtdCombustivel;
+    }
+
+}
---
+++ LOIANNE GRONER/JAVA/primeiro-projeto/src/com/loiane/cursojava/construtores/Carro2.java
@@ -0,0 +1,17 @@
+package com.loiane.cursojava.construtores;
+
+public class Carro2 {
+    public String marca;
+    public String modelo;
+    public int numPassageiros;
+    public double capCombustivel;
+    public double consCombustivel;
+
+    public Carro2(String marca, String modelo, int numPassageiros, double capCombustivel, double consCombustivel) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.numPassageiros = numPassageiros;
+        this.capCombustivel = capCombustivel;
+        this.consCombustivel = consCombustivel;
+    }
+}
