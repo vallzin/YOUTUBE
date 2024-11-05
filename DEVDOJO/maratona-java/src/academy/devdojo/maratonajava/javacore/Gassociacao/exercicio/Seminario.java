acef4097bf30f30288cd34d31b97ab141e094fbe
vallzin <valmilson@gmail.com>
2024-11-05 11:08:23 -0300

    refatoração Associação parte05

--- DEVDOJO/maratona-java/src/academy/devdojo/maratonajava/javacore/Gassociacao/exercicio/Seminario.java
+++ DEVDOJO/maratona-java/src/academy/devdojo/maratonajava/javacore/Gassociacao/exercicio/Seminario.java
@@ -7,15 +7,12 @@ public class Seminario {
     private Local local;
 
 
-    public Seminario(String titulo) {
+    public Seminario(String titulo, Local local) {
         this.titulo = titulo;
+        this.local = local;
 
     }
 
-    public Seminario(String titulo, Aluno[] alunos) {
-        this.titulo = titulo;
-        this.alunos = alunos;
-    }
 
     public Seminario(String titulo, Aluno[] alunos, Local local) {
         this.titulo = titulo;
