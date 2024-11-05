3bd34399e09f59a17e83ac642e68d317a3a6fcb6
vallzin <valmilson@gmail.com>
2024-11-05 08:21:30 -0300

    devdojo 'Associação parte01'

---
+++ DEVDOJO/maratona-java/src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Jogador.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.dominio;
+
+public class Jogador {
+
+    private String nome;
+
+    public Jogador(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void imprimir() {
+        System.out.println(this.nome);
+    }
+}
---
+++ DEVDOJO/maratona-java/src/academy/devdojo/maratonajava/javacore/Gassociacao/test/JogadorTest.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.test;
+
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Jogador;
+
+public class JogadorTest {
+
+    public static void main(String[] args) {
+
+        Jogador j1 = new Jogador("Pelé");
+        Jogador j2 = new Jogador("Romário");
+        Jogador j3 = new Jogador("Cafú");
+
+        Jogador[] jogadores = {j1, j2, j3};
+
+        for (Jogador j : jogadores){
+            j.imprimir();
+        }
+
+    }
+
+}
