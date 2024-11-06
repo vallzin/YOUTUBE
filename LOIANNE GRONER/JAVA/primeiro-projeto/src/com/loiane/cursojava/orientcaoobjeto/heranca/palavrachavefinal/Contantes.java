17a30ec8c76b830e70392b7f36ca0a630ed5cb65
vallzin <valmilson@gmail.com>
2024-11-06 17:08:12 -0300

    loiane palavra chave final

---
+++ LOIANNE GRONER/JAVA/primeiro-projeto/src/com/loiane/cursojava/orientcaoobjeto/heranca/palavrachavefinal/Contantes.java
@@ -0,0 +1,6 @@
+package com.loiane.cursojava.orientcaoobjeto.heranca.palavrachavefinal;
+
+public final class Contantes {
+    public static final String URL_BLOG = "http://meublog.com";
+
+}
---
+++ LOIANNE GRONER/JAVA/primeiro-projeto/src/com/loiane/cursojava/orientcaoobjeto/heranca/palavrachavefinal/HerancaTest4.java
@@ -0,0 +1,16 @@
+package com.loiane.cursojava.orientcaoobjeto.heranca.palavrachavefinal;
+
+public class HerancaTest4 {
+
+    public static void main(String[] args) {
+
+        Pessoa pessoa = new Pessoa();
+//        Pessoa aluno = new Aluno();
+        Aluno aluno = new Aluno();
+//        Pessoa prof = new Professor();
+        Professor prof = new Professor();
+
+        System.out.println(Contantes.URL_BLOG);
+
+    }
+}
