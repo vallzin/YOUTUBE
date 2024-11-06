9f3db9a671dbeb0aeff9c43b8934f69bc9c49d19
vallzin <valmilson@gmail.com>
2024-11-06 09:04:31 -0300

    devdojo Herança sobrescrita

---
+++ DEVDOJO/maratona-java/src/academy/devdojo/maratonajava/javacore/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Isobrescrita.test;
+
+import academy.devdojo.maratonajava.javacore.Isobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+
+        Anime anime = new Anime("Homem Aranha");
+
+        System.out.println(anime);
+
+    }
+}
