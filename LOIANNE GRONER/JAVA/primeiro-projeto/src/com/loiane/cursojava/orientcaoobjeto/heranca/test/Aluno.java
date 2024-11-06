9f4568a488c1e5ff8488c76f9c7b5fe5512800d9
vallzin <valmilson@gmail.com>
2024-11-06 15:21:00 -0300

    Herança palavra chave super

---
+++ LOIANNE GRONER/JAVA/primeiro-projeto/src/com/loiane/cursojava/orientcaoobjeto/heranca/test/Aluno.java
@@ -0,0 +1,50 @@
+package com.loiane.cursojava.orientcaoobjeto.heranca.test;
+
+public class Aluno extends Pessoa {
+
+    private String curso;
+    private double[] notas;
+
+    public Aluno() {
+        super();
+    }
+
+    public Aluno(String nome, String endereco,
+                 String telefone, String curso,
+                 double[] notas) {
+        super(nome, endereco, telefone);
+        this.curso = curso;
+        this.notas = notas;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public double[] getNotas() {
+        return notas;
+    }
+
+    public void setNotas(double[] notas) {
+        this.notas = notas;
+    }
+
+    public double calcularMedia(){
+        return 0;
+    }
+
+    public boolean verificarAprovado(){
+        return true;
+    }
+
+    public void metodoQualquer(){
+        super.setCpf("4567897956");
+
+        this.setCpf("78896546");
+    }
+
+}
---
+++ LOIANNE GRONER/JAVA/primeiro-projeto/src/com/loiane/cursojava/orientcaoobjeto/heranca/test/HerancaTest.java
@@ -0,0 +1,31 @@
+package com.loiane.cursojava.orientcaoobjeto.heranca.test;
+
+public class HerancaTest {
+
+    public static void main(String[] args) {
+
+        /*
+        Aluno aluno = new Aluno();
+
+        Professor prof = new Professor();
+
+        Pessoa pessoa = new Pessoa();
+
+        aluno.setNome("Maria");
+
+        prof.setTelefoneCelular("123456789");
+
+        */
+
+        Pessoa aluno = new Aluno();
+
+        //aluno.getNome();
+
+        Pessoa professor = new Pessoa();
+        //professor.getNome();
+
+    }
+
+
+
+}
