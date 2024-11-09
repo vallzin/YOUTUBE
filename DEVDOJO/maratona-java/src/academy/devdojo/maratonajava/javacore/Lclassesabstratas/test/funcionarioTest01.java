281696403cee90cdec08c1f5386887f5d4b44a8f
vallzin <valmilson@gmail.com>
2024-11-09 10:36:41 -0300

    devdojo classes abstratas parte01

---
+++ DEVDOJO/maratona-java/src/academy/devdojo/maratonajava/javacore/Lclassesabstratas/test/funcionarioTest01.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.Lclassesabstratas.test;
+
+import academy.devdojo.maratonajava.javacore.Lclassesabstratas.dominio.Desenvolvedor;
+import academy.devdojo.maratonajava.javacore.Lclassesabstratas.dominio.Funcionario;
+import academy.devdojo.maratonajava.javacore.Lclassesabstratas.dominio.Gerente;
+
+public class funcionarioTest01 {
+
+    public static void main(String[] args) {
+
+//        Funcionario funcionario = new Funcionario("Zorro",2000);
+        Gerente gerente = new Gerente("Fulêro",20000);
+        Desenvolvedor desenvolvedor = new Desenvolvedor("Caguya", 2500);
+
+
+//        System.out.println(funcionario);
+        System.out.println(gerente);
+        System.out.println(desenvolvedor);
+    }
+}
