dd8aba56b525dc5ac5809315cb2344f0cae709e0
vallzin <valmilson@gmail.com>
2024-11-11 10:07:34 -0300

    devdojo polimorfismo parte03

--- DEVDOJO/maratona-java/src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/ProdutoTest01.java
+++ DEVDOJO/maratona-java/src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/ProdutoTest01.java
@@ -1,6 +1,7 @@
 package academy.devdojo.maratonajava.javacore.Npolimorfismo.test;
 
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Televisao;
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.CalculadorImposto;
 
@@ -9,12 +10,15 @@ public class ProdutoTest01 {
     public static void main(String[] args) {
 
         Computador comp = new Computador("NUC10i7", 11000);
-
         Tomate tomate = new Tomate("Tomate Siciliano", 10);
+        Televisao tv = new Televisao("Samsung 50\" ", 5000);
 
-        CalculadorImposto.calcularImpostoComputador(comp);
+
+        CalculadorImposto.calcularImposto(comp);
         System.out.println("-*- ----------------- -*-");
-        CalculadorImposto.calcularImpostoCTomate(tomate);
+        CalculadorImposto.calcularImposto(tomate);
+        System.out.println("-*- ----------------- -*-");
+        CalculadorImposto.calcularImposto(tv);
 
 
 
