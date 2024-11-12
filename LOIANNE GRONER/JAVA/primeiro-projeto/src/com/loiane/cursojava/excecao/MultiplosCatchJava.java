eb7211f2b553f8de4a11fd1db5a179ade1a86e48
vallzin <valmilson@gmail.com>
2024-11-12 07:51:41 -0300

    loiane exceptions: multiplos catch

---
+++ LOIANNE GRONER/JAVA/primeiro-projeto/src/com/loiane/cursojava/excecao/MultiplosCatchJava.java
@@ -0,0 +1,18 @@
+package com.loiane.cursojava.excecao;
+
+public class MultiplosCatchJava {
+
+    public static void main(String[] args) {
+
+        int[] numeros = {4, 8, 16, 32, 64, 128};
+        int[] demon = {2, 0, 4, 8, 0};
+
+        for (int i = 0; i < numeros.length; i++){
+            try {
+                System.out.println(numeros[i] + "/" + demon[i] + " = " + (numeros[i] / demon[i]));
+            }catch (ArithmeticException | ArrayIndexOutOfBoundsException e1){
+                System.out.println("Erro");
+            }
+        }
+    }
+}
