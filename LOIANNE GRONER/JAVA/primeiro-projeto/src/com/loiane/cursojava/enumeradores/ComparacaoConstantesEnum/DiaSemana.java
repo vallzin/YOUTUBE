788fc1b70fe0061787c34650666a18b44cb55d6a
vallzin <valmilson@gmail.com>
2024-11-22 10:06:27 -0300

    loiane Enum introdução

---
+++ LOIANNE GRONER/JAVA/primeiro-projeto/src/com/loiane/cursojava/enumeradores/ComparacaoConstantesEnum/DiaSemana.java
@@ -0,0 +1,13 @@
+package com.loiane.cursojava.enumeradores.ComparacaoConstantesEnum;
+
+public enum DiaSemana {
+
+    SEGUNDA,
+    TERCA,
+    QUARTA,
+    QUINTA,
+    SEXTA,
+    SABADO,
+    DOMINGO;
+
+}
