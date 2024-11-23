5bdc38fa8bf023bf7e18fb612b559c2c1961d48d
vallzin <valmilson@gmail.com>
2024-11-23 18:45:36 -0300

    devdojo string parte05

---
+++ DEVDOJO/maratona-java/src/academy/devdojo/maratonajava/javacore/Qstring/StringBuilderTest01.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.Qstring;
+
+public class StringBuilderTest01 {
+    public static void main(String[] args) {
+        String nome = "valmilson";
+        nome = nome.concat(" lucena");
+        System.out.println(nome);
+        StringBuilder sb = new StringBuilder("valmilson lucena");
+        sb.append(" aluno").append(" java");
+        sb.
+        System.out.println(sb);
+
+    }
+}
